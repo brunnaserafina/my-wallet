c4fffc638a7ad695a733d3ff80d7c8ab56f55c60
Brunna Serafina <brunnaserafina@gmail.com>
2022-09-05 23:47:09 -0300

    feat: creating Login Page in route '/'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PageLogin from "./components/PageLogin";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes></Routes>
+        <Routes>
+          <Route path="/" element={<PageLogin />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
