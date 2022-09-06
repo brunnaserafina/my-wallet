0c06baa883141249b55cc667fe04689dda61c57f
Brunna Serafina <brunnaserafina@gmail.com>
2022-09-06 12:24:59 -0300

    style: styling SignUp Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PageLogin from "./components/PageLogin";
+import PageSignUp from "./components/PageSignUp";
 import "./assets/css/reset.css";
 
 export default function App() {
