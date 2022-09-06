db918d336ddef39e1e231c04a456717ece4e6169
Brunna Serafina <brunnaserafina@gmail.com>
2022-09-06 12:08:36 -0300

    feat: creating login functionality in route (POST '/')

--- src/components/PageLogin.js
+++ src/components/PageLogin.js
@@ -1,14 +1,58 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 export default function PageLogin() {
+  const navigate = useNavigate("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const body = {
+    email,
+    password,
+  };
+
+  function joinLogin(event) {
+    event.preventDefault();
+
+    const requisicao = axios.post("http://localhost:5000/", body);
+
+    requisicao.catch(() => {
+      alert("Email ou senha incorreta!");
+      setEmail("");
+      setPassword("");
+    });
+
+    requisicao.then((response) => {
+      console.log(response);
+      navigate("/registros");
+    });
+  }
+
   return (
     <HomeContainer>
       <Logo>MyWallet</Logo>
-      <Input placeholder="E-mail" />
-      <Input placeholder="Senha" />
-      <Button>Entrar</Button>
-      <Link to={"/cadastro"}>
+
+      <form onSubmit={joinLogin}>
+        <Input
+          placeholder="E-mail"
+          value={email}
+          type="email"
+          pattern="[a-z0-9._%+-]+@[a-zLink, 0-9.-]+\.[a-z]{2,4}$"
+          required
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Senha"
+          value={password}
+          type="password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button>Entrar</Button>
+      </form>
+
+      <Link to={`/cadastro`}>
         <p>Primeira vez? Cadastre-se</p>
       </Link>
     </HomeContainer>
@@ -23,6 +67,13 @@ const HomeContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 
   a {
     font-size: 15px;
